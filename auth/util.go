9aac7e1fbe08b18e9355c942bd80e7f04f84855a
nvcnvn <nguyen@open-vn.org>
2014-05-24 11:18:23 +0700

    better error response

--- auth/util.go
+++ auth/util.go
@@ -1,33 +1,48 @@
 package auth
 
 import (
+	"encoding/json"
 	"net/http"
 	"strings"
 )
 
-func BadRequestHanlder(rw http.ResponseWriter, req *http.Request) {
-	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
-	rw.WriteHeader(http.StatusBadRequest)
-	rw.Write([]byte(`{"error":"Badd Request"}`))
+const (
+	ErrCodeUnknown   int = 0
+	ErrCodeNotLogged     = 1
+)
+
+// JSONErr returned to the user an informative error json message
+type JSONErr struct {
+	Code        int
+	Message     string
+	Description string    `json:",omitempty"`
+	StackStrace string    `json:",omitempty"`
+	Errors      []JSONErr `json:",omitempty"`
 }
 
-func ForbiddenHanlder(rw http.ResponseWriter, req *http.Request) {
+func errHandler(rw http.ResponseWriter, sttCode int, err *JSONErr) {
 	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
-	rw.WriteHeader(http.StatusForbidden)
-	rw.Write([]byte(`{"error":"` + ErrNotLogged.Error() + `"}`))
+	rw.WriteHeader(sttCode)
+	json.NewEncoder(rw).Encode(err)
 }
 
-func InternalServerErrorHanlder(rw http.ResponseWriter, req *http.Request) {
-	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
-	rw.WriteHeader(http.StatusInternalServerError)
-	rw.Write([]byte(`{"error":"Internal Server Error"}`))
+func BadRequestHanlder(rw http.ResponseWriter, err *JSONErr) {
+	errHandler(rw, http.StatusBadRequest, err)
+}
+
+func ForbiddenHanlder(rw http.ResponseWriter, err *JSONErr) {
+	errHandler(rw, http.StatusForbidden, err)
+}
+
+func InternalServerErrorHandler(rw http.ResponseWriter, err *JSONErr) {
+	errHandler(rw, http.StatusInternalServerError, err)
 }
 
 func OAuthHandleWrapper(handler http.HandlerFunc, pri ...string) http.HandlerFunc {
 	return func(rw http.ResponseWriter, req *http.Request) {
 		userMngr, err := Provider().OpenUserMngr()
 		if err != nil {
-			InternalServerErrorHanlder(rw, req)
+			InternalServerErrorHandler(rw, &JSONErr{Message: err.Error()})
 		}
 		defer userMngr.Close()
 
@@ -35,18 +50,34 @@ func OAuthHandleWrapper(handler http.HandlerFunc, pri ...string) http.HandlerFun
 		user, err := userMngr.GetUser(token)
 		if err != nil {
 			if err == ErrNotLogged {
-				ForbiddenHanlder(rw, req)
+				ForbiddenHanlder(rw, &JSONErr{
+					Code:        ErrCodeNotLogged,
+					Message:     err.Error(),
+					Description: "User need to be logged in to perform this action.",
+				})
 				return
 			}
-			InternalServerErrorHanlder(rw, req)
+			InternalServerErrorHandler(rw, &JSONErr{Message: err.Error()})
 			return
 		}
 
-		for _, val := range pri {
-			if !userMngr.Can(user, val) {
-				ForbiddenHanlder(rw, req)
-				return
+		var (
+			cannot   bool
+			cannotDo string
+		)
+		for _, do := range pri {
+			if !userMngr.Can(user, do) {
+				cannotDo += do + ","
 			}
+		}
+
+		if cannot {
+			ForbiddenHanlder(rw, &JSONErr{
+				Code:        2,
+				Message:     "User doesn't have valid permission.",
+				Description: "User doesn't have " + cannotDo + " permission.",
+			})
+			return
 		}
 
 		// run user defined handler
